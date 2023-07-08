cbba32fac22628174b5e83943ae0f46555a68c45
Samsam <samsamtaibi@gmail.com>
2023-07-08 15:10:52 +0200

    version ggle

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 terraform {
   required_providers {
-    google = {source = "hashicorp/google"}
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0" }
   }
 }
 provider "google" {
